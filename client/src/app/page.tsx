a3b61ed1e8d6646d0c536c7acd507dce2b365cc2
yassine <yassinechmarekh88@gmail.com>
2025-10-30 23:29:29 +0100

    initialization client with next setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Container from "@/components/container";
+
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  );
+}
---
+++ client/src/components/container.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Container = ({ children, className }: ContainerProps) => {
+  return (
+    <div
+      className={cn(
+        "mx-4 sm:mx-8 md:mx-12 lg:mx-16 xl:mx-32 2xl:mx-auto 2xl:max-w-384 bg-red-500",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
